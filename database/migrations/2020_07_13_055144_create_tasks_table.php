a1af4513e45a4a3d47ee54623b52ba42b3b9fa1c
Eduardo Perez <lalo383596@gmail.com>
2020-07-13 01:09:49 -0500

    first commit

---
+++ database/migrations/2020_07_13_055144_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments(column'id');
+            $table->integer(column'user_id')->unsigned();
+            $table->integer (column'title');
+            $table->timestamps();
+            $table->foreign(column'user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
