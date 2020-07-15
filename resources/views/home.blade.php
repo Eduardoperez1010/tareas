2af30eeed0eefd67333dbe0cb5fe4c6d10f32a11
Eduardo Perez <lalo383596@gmail.com>
2020-07-15 12:14:19 -0500

    Agregando una nueva vista para el registro y autenticacion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Bienvenido estas en el sistema!') }}
                 </div>
             </div>
         </div>
