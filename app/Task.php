efe7cd082f7541037acc64d97309fa570a716495
Eduardo Perez <lalo383596@gmail.com>
2020-07-16 03:42:35 -0500

    Error solucionado....!

--- app/Task.php
+++ app/Task.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-	protected $table = ['tasks'];
+	protected $table = 'tasks';
     protected $fillable = ['title'];
 
     public function user(){
