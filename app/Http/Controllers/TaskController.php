32b8a0fc8206e3c21cbf833cb42c6b3eceb381c0
Eduardo Perez <lalo383596@gmail.com>
2020-07-16 02:25:08 -0500

    Agregando el controldaor tareas para la Autenticacion

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+	public function __construct(){
+        $this->middleware('auth');
+    }
 }
